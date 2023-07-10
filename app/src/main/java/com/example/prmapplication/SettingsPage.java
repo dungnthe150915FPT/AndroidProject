02960cedc9554b7775385f1ae421b0df59f23bcb
quanvhmhe150859 <quanvhmhe150859@fpt.edu.vn>
2023-07-10 22:58:22 +0700

    night mode switch

--- app/src/main/java/com/example/prmapplication/SettingsPage.java
+++ app/src/main/java/com/example/prmapplication/SettingsPage.java
@@ -1,14 +1,56 @@
 package com.example.prmapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class SettingsPage extends AppCompatActivity {
+
+    Switch switchNight;
+    boolean nightMode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    public void BindingView(){
+        switchNight = findViewById(R.id.nightswitcher);
+
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMode = sharedPreferences.getBoolean("night", false);
+
+        if(nightMode){
+            switchNight.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+    }
+
+    public void BindingAction(){
+        switchNight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(nightMode){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night", false);
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night", true);
+                }
+                editor.apply();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
+        BindingView();
+        BindingAction();
     }
 }
